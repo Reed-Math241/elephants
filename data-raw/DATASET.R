e7b64fac55516c568cfd672bcc8763d3bc05855f
gmcginnis <gmcginnis@reed.edu>
2021-02-13 00:02:04 -0800

    Added col specification for one mistagged var. Wrangled data to change obs marked "no data" to NA, and converted those vars to logical.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,26 @@
 library(janitor)
 library(tidyverse)
 
-## code to prepare `DATASET` dataset goes here
+# Downloading raw data
 
-ea_elephants_raw <- readr::read_csv("https://ufdcimages.uflib.ufl.edu/IR/00/00/42/09/00001/ParkerElephantData_edition02.csv")
+ea_elephants_raw <- readr::read_csv(
+  "https://ufdcimages.uflib.ufl.edu/IR/00/00/42/09/00001/ParkerElephantData_edition02.csv",
+  col_types = cols(Parasites = col_character())
+  )
+
+# Wrangling data
 
 ea_elephants <- ea_elephants_raw %>%
   clean_names() %>%
-  mutate(date_collect = as.Date(date_collect, format = "%m/%d/%Y"))
+  mutate(date_collect = as.Date(date_collect, format = "%m/%d/%Y")) %>%
+  mutate(across(c(lactating, anestrus, pregnant), na_if, "no data")) %>%
+  mutate_at(
+    c("lactating", "anestrus", "pregnant"),
+    funs(case_when(
+      . == "Yes" ~ TRUE,
+      . == "No" ~ FALSE
+      ))
+    )
+
 
 usethis::use_data(DATASET, overwrite = TRUE)
