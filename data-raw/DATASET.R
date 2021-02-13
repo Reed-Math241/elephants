d6b11d5e6cd7c9fccd5bea21b42ae8150d637c17
gmcginnis <gmcginnis@reed.edu>
2021-02-12 23:11:37 -0800

    Listed libraries; wrangled to change date_collect class Character variable to class Date

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,12 @@
+library(janitor)
+library(tidyverse)
+
 ## code to prepare `DATASET` dataset goes here
 
-ea_elephants <- readr::read_csv("https://ufdcimages.uflib.ufl.edu/IR/00/00/42/09/00001/ParkerElephantData_edition02.csv")
+ea_elephants_raw <- readr::read_csv("https://ufdcimages.uflib.ufl.edu/IR/00/00/42/09/00001/ParkerElephantData_edition02.csv")
 
-ea_elephants <- janitor::clean_names(ea_elephants)
+ea_elephants <- ea_elephants_raw %>%
+  clean_names() %>%
+  mutate(date_collect = as.Date(date_collect, format = "%m/%d/%Y"))
 
 usethis::use_data(DATASET, overwrite = TRUE)
