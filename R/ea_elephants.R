4f890fe621f01486ef9baedc59a28be7b3b43e26
paulhnguyen <paulh_ngyuen@yahoo.com>
2021-02-16 17:03:42 -0800

    finished data dictionary, updated # observations

--- R/ea_elephants.R
+++ R/ea_elephants.R
@@ -1,11 +1,11 @@
 #' The Ian Parker East African Elephant Data Set
 #' 
-#' East African elephant (Loxodonta africana) biological data from 3,175 field record sheets. The data were recorded from 1965 to 1969 during herd culling designed to mitigate elephant herd overpopulation at multiple environmentally stressed national park sites. 
+#' East African elephant (Loxodonta africana) biological data from 3,175 field record sheets (in some instances, multiple cases were assigned to a single sheet). The data were recorded from 1965 to 1969 during herd culling designed to mitigate elephant herd overpopulation at multiple environmentally stressed national park sites. The data are unique in several ways: the conditions in which such a large number of elephants were represented are unlikely to be reproduced; the sampling represents natural (albeit stressed) family groups rather than trophies or weak animals (as is generally true in managed environments). From The Ian Parker East African Elephant Data Sheets: A handbook for the transcribed biological data set.
 #' 
 #' @docType data
 #' @name ea_elephants
 #' @usage data(ea_elephants)
-#' @format  A data frame with 3307 observations on the following 100 variables.
+#' @format  A data frame with 3306 observations on the following 100 variables.
 #' \itemize{
 #' \item{\code{Template_ID}}{ (Template version): Internal variable indicates REDCap online template version employed by transcriber.}
 #' \item{\code{REDCap_ID}}{ (Record ID): Internal variable, REDCap system number programmatically assigned.}
@@ -34,10 +34,82 @@
 #' \item{\code{Tusks_totlengthL}}{ (Tusks, total length, Left): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
 #' \item{\code{Foreft_diamR}}{ (Forefoot diameter, Right): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
 #' \item{\code{Foreft_diamL}}{ (Forefoot diameter: Left): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{Hindft_diamR}}{ (Hindfoot diameter: Right): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{Hindft_diamL}}{ (Hindfoot diameter: Left): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{Foreft_circumR}}{ (Forefoot circumference: Right): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{Foreft_circumL}}{ (Forefoot circumference: Left): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{Hindft_circumR}}{ (Hindfoot circumference: Right): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{Hindft_circumL}}{ (Hindfoot circumference: Left): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{ToeNforeR}}{ (Toenails, number of, fore, right): simple count, ranged from 3-5.}
+#' \item{\code{ToeNforeL}}{ (Toenails, number of, fore, left): simple count, ranged from 3-5.}
+#' \item{\code{ToeNhindR}}{ (Toenails, number of, hind, right): simple count, ranged from 3-5.}
+#' \item{\code{ToeNhindL}}{ (Toenails, number of, hind, left): simple count, ranged from 3-5.}
+#' \item{\code{Tusk_circumRlip}}{ (Tusk circumference at lip, Right): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{Tusk_circumLlip}}{ (Tusk circumference at lip, Left): Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{Notes_body}}{ (Notes for body section): Sheet notes on length measurements such as body, trunk, tusks, feet, or number of toes.}
+#' \item{\code{Hindleg_Lwght}}{ (Left hind leg, weight, if added by hand): Original measurement units in pounds or kilograms converted to grams during transcription.}
+#' \item{\code{Stomach_full}}{ (Stomach, full): Original measurement units in pounds or kilograms converted to grams during transcription.}
+#' \item{\code{Stomach_empty}}{ (Stomach, empty): Original measurement units in pounds or kilograms converted to grams during transcription.}
+#' \item{\code{Stomach_fill}}{ (Stomach, fill): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Intestine_full}}{ (Intestine, full): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Intestine_empty}}{ (Intestine, empty): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Intestine_fill}}{ (Intestine fill): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Kidneys+fatR}}{ (Kidneys, + fat, right if separate): Organ weight with fat if recorded separately, otherwise total given for both below. Original measurement units in pounds or grams converted to grams during transcription where necessary.}
+#' \item{\code{Kidneys+fatL}}{ (Kidneys, + fat, left if separate): Organ weight with fat if recorded separately, otherwise total for both organs. Original measurement units in pounds or grams converted to grams during transcription where necessary.}
+#' \item{\code{Kidneys+fatTot}}{ (Kidneys, + fat; total if written as single weight). Original measurement units in pounds or grams converted to grams during transcription where necessary.}
+#' \item{\code{Kidneys-fatR}}{ (Kidneys, - fat; weight, right). Organ weight without fat. Original measurement units in pounds or grams converted to grams during transcription where necessary.}
+#' \item{\code{Kidneys-fatL}}{ (Kidneys, - fat; weight, left). Organ weight without fat. Original measurement units in pounds or grams converted to grams during transcription where necessary.}
+#' \item{\code{Kidneys-fatTot}}{ (Kidneys, - fat; total if written as single weight). Original measurement units in pounds or grams converted to grams during transcription where necessary.}
+#' \item{\code{Temporal_glandR}}{ (Temporal gland, right): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Temporal_glandL}}{ (Temporal gland, left): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Brain}}{ (Brain): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Adrenals_wghtR}}{ (Adrenals, right if separate): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Adrenals_wghtL}}{ (Adrenals, left if separate): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Adrenals_wghtTot}}{ (Adrenals; total if written as single weight): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Thyroid}{ (Thyroid): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Heart_wght}}{ (Heart): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Lungs_trachea}}{ (Lungs & trachea): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Liver_wght}}{ (Liver): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Spleen_wght}}{ (Spleen): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Tusk_wghtR}}{ (Tusks, right): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Tusk_wghtL}}{ (Tusks, left): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Notes_wghts}}{ (Transcriber or sheet recorder notes on weights): Notes by recorder or transcriber.}
+#' \item{\code{Lactating}}{ (Lactating): Check or tick mark on original sheet indicated ‘yes’; dash or strike indicated ‘no’; blank indicated ‘no data.’}
+#' \item{\code{Anestrus}}{ (Anestrus): Check or tick mark on original sheet indicated ‘yes’; dash or strike indicated ‘no’; blank indicated ‘no data.’}
+#' \item{\code{Pregnant}}{ (Pregnant): Check or tick mark on original sheet indicated ‘yes’; dash or strike indicated ‘no’; blank indicated ‘no data.’}
+#' \item{\code{Fetus_sex}}{ (Foetus: sex): Sex if fetus present.}
+#' \item{\code{Fetus_lngth}}{ (Foetus: length): Original measurement units in inches, centimeters, or millimeters converted to inches during transcription if necessary..}
+#' \item{\code{Fetus_wght}}{ (Foetus: weight): Original measurement units in pounds or grams converted to grams during transcription if necessary.}
+#' \item{\code{Fetus_umbilicus}}{ (Foetus: umbilicus): Original measurement units in inches or centimeters converted to inches during transcription if necessary.}
+#' \item{\code{Mammary_gland}}{ (Mammary gland): Original measurement units in pounds or grams converted to grams during transcription if necessary.}
+#' \item{\code{OvariesR}}{ (Ovaries right): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{OvariesL}}{ (Ovaries left): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Uterus+}}{ Organ weight plus accompanying fat. Original measurement units in pounds or grams converted to grams during transcription if necessary.}
+#' \item{\code{Uterus-}}{ (Uterus -): Organ weight without fat. Original measurement units in pounds or grams converted to grams during transcription if necessary.}
+#' \item{\code{Conceptus}}{ (Conceptus): Original measurement units in pounds converted to grams during transcription.}
+#' \item{\code{Placenta}}{ (Placenta): Original measurement units in pounds or grams converted to grams during transcription if necessary.}
+#' \item{\code{Parasites}}{ (Parasites, if hand written onto form): notes field.}
+#' \item{\code{Fetus}}{ Original measurement units in pounds or grams converted to grams during transcription if necessary.}
+#' \item{\code{Sperm_smear}}{ (Sperm smear): note field on results of procedure.}
+#' \item{\code{TestesR}}{ (Testes Right): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{TestesL}}{ (Testes Left): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Sem_VesiclesR}}{ (Seminal vesicles Right): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Sem_VesiclesL}}{ (Seminal vesicles Left): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{BulboUrethrR}}{ (Bulbo-urethral gl. Right): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{BulboUrethrL}}{ (Bulbo-urethral gl. Left): Original measurement units in grams; no conversion necessary.}
+#' \item{\code{Notes_FieldReprod}}{ (Record any reproductive notes from data sheet here): Transcribed notes field.}
+#' \item{\code{Notes_other}}{ (Record any other notes not transcribed above from data sheet here): Transcribed notes field.}
+#' \item{\code{Notes_TranscReprod}}{ (Transcribers notes on reproduction section): Notes input by transcriber if issues or questions.}
+#' \item{\code{Tot_wght}}{ (Total weight): Original measurement units in pounds converted to grams.}
+#' \item{\code{Live_wght}}{ (Live weight): Original measurement units in pounds or kilograms converted to grams during transcription.}
+#' \item{\code{Notes_StomachContent}}{ (Record any notes under 'Stomach contents' or any other 'Notes' here): Transcribed notes field.}
+#' \item{\code{Notes_transcriber}}{ (Transcriber notes): Notes input by transcriber if issues or questions.}
+#' \item{\code{Collector}}{ (Collector): Name of data collector recorded if available (Budongo only).}
+#' \item{\code{EarTail_lngth}}{ (Ear - tail): Measurement for Budongo cases only. Original measurement units in feet and inches or centimeters converted to total inches during transcription.}
+#' \item{\code{Aorta_wght}}{ (Aorta lbs.): Original measurement units in pounds converted to grams during transcription.}
 #' }
-#' @source 
-#' Data collected by Candice Corvetti and used in the "Stat 101" case study 
-#' "BLABLABLA".  
-#' See also 
-#' <https://www.saratogacountyny.gov/departments/real-property-tax-service-agency/>
+#' @source {The Ian Parker East African Elephant Data Sheets: A handbook for the transcribed biological data set. 2014.} \url{https://ufdc.ufl.edu/IR00004209/00001}
+#' Data collected by Wildlife Services, Ltd., Donated to the University of Florida by Ian S.C. Parker in 2010, and transcribed to a machine readable format in 2013.
+#' "The Ian Parker East African Elephant Data Sheets".  
+
 "ea_elephants"
